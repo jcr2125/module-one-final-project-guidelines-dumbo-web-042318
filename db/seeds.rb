fdca8d40b0ae2423c4ce773508794d3946952d9c
travisdock <travisdock@gmail.com>
2018-05-09 16:31:17 -0400

    add code to seeds.rb to seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
 require 'rest-client'
 require 'JSON'
-require 'pry'
 
 github = JSON.parse(RestClient.get('https://api.github.com/search/repositories?q=topic:oop'))
-
-binding.pry
+repos_array = github["items"]
+repos_array.each do |repo|
+  User.create(name: repo["owner"]["login"])
+  Language.create(name: repo["language"])
+  Repository.create(name: repo["name"], description: repo["description"], forks: repo["forks"], watchers: repo["watchers"])
+end
 
 true
