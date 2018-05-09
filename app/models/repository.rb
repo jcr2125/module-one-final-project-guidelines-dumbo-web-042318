0e73c24c66b48f5e94e6635f1b209b2a328dc710
travisdock <travisdock@gmail.com>
2018-05-09 14:32:41 -0400

    add associations for models of language, repository, user, and update environment with require app directory

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,3 +1,6 @@
 class Repository < ActiveRecord::Base
-
+  belongs_to :owner, :class_name => "User"
+  has_many :contributors, through: :repo_contrs, :class_name => "User"
+  belongs_to :language
+  has_many :repo_contrs
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
-
+  #as owner
+  has_many :repositories, :foreign_key => 'owner_id'
+  has_many :languages, through: :repositories
+  #as contributor
+  has_many :repo_contrs, :foreign_key => 'contributor_id'
+  has_many :repositories, through: :repo_contrs
 end
